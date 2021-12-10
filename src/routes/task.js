470d42c005ac5612a43b29815bcfd4e52ff36761
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-12-10 06:06:00 +0530

    added new fiel d  referenced that field to the user

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 const {
@@ -9,14 +10,14 @@ const {
   tensai,
 } = require("../controllers/task");
 
-router.post("/tasks", create);
+router.post("/tasks", auth, create);
 
-router.get("/tasks", fetchAll);
+router.get("/tasks", auth, fetchAll);
 
-router.get("/tasks/:id", fetch);
+router.get("/tasks/:id", auth, fetch);
 
-router.patch("/tasks/:id", update);
+router.patch("/tasks/:id", auth, update);
 
-router.delete("/tasks/:id", tensai);
+router.delete("/tasks/:id", auth, tensai);
 
 module.exports = router;

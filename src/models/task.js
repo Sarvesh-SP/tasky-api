afcd7d31b9a9d71f7e00f1bd7f6ce092d37a7970
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-12-15 19:19:50 +0530

    added timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = Task;
+module.exports = mongoose.model("Task", taskSchema);

753a31bbc3d3893a7ab3ac72cc7b90477e9bb108
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-12-21 20:39:26 +0530

    modified dir for testing purpose

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const connect = require("./config/db");
+const app = require("./app");
 
-//routers
-const userRouter = require("./routes/user");
-const taskRouter = require("./routes/task");
-
-connect();
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
 app.listen(port, () => {
-  console.log(`Server is up on port http://localhost:${port}`);
+	console.log(`Server is up on port http://localhost:${port}`);
 });

0ba7365d21b4a4e1a715d048d74539607b3d464a
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-12-21 20:38:15 +0530

    modified db

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const validator = require("validator");
 const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_PROD, {
-      useNewUrlParser: true,
-    })
-    .then(() => {
-      console.log("Connect DB");
-    });
+	mongoose
+		.connect(process.env.MONGO_PROD, {
+			useNewUrlParser: true,
+		})
+		.then(() => {
+			console.log("Connect DB");
+		});
 };
 
 module.exports = connectDB;
